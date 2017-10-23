bb8dce19085d615302e5613cde0ff8d3bde8e5d5
bartek007 <barzur001@utp.edu.pl>
2017-10-23 11:31:01 +0200

    0.0.2

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -17,6 +17,7 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Wersja 0.0.1");
+        System.out.println("Wersja 0.0.2");
     }
     
 }
